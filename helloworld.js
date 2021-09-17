50b7c62054763b76f112f5f217f4e2ef7c33b4ca
VVJS-22 <s.jayeshchennai@gmail.com>
2021-09-17 19:03:27 +0530

    second commit

--- helloworld.js
+++ helloworld.js
@@ -8,3 +8,37 @@ console.log(23)
 
 console.log(message)
 
+var x;
+const y = "javascript";
+let z;
+
+const object = {
+    name: "Kalai",
+    age: 22,
+    gender: "Male"
+}
+
+
+
+console.log(x,y,z)
+
+/*
+Data          Types	Description	                                            Example
+String	   represents textual data	                              'hello', "hello world!" etc
+Number	   an integer or a floating-point number	               3, 3.234, 3e-2 etc.
+BigInt	   an integer with arbitrary precision	                   900719925124740999n , 1n etc.
+Boolean	   Any of two values: true or false	                       true and false
+undefined   a data type whose variable is not initialized	       let a;
+null	       denotes a null value	let a = null;
+Symbol	   data type whose instances are unique and immutable	   let value = Symbol('hello');
+Object	   key-value pairs of collection of data
+*/
+
+
+/*
+commit an update 
+
+git add .
+git commit -m "commit message"
+git push
+*/
