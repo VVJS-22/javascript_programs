4aeb3c2b0593bf4818d19227e63c04be00995619
VVJS-22 <s.jayeshchennai@gmail.com>
2021-09-22 19:23:15 +0530

    7th class

--- arrays.js
+++ arrays.js
@@ -1,44 +1,62 @@
-let array = [] //empty array
+// let array = [] //empty array
 
-console.log(array)
+// console.log(array)
 
-array = [1, 2, 3, 4, 5]
+// array = [1, 2, 3, 4, 5]
 
-console.log(array)
+// console.log(array)
 
-console.log(array.indexOf(3)) // return index
+// console.log(array.indexOf(3)) // return index
 
-console.log(array[2]) // return the element
+// console.log(array[2]) // return the element
 
-console.log(array.length)
+// console.log(array.length)
+
+// array = [3, "string", 5.32, true, undefined, null, {name: "John", age: 15}]
+
+// console.log(array)
+
+// console.log(array.pop())
+// console.log(array)
+
+// console.log(array.shift())
+// console.log(array)
+
+// array.push(143)
+// array.unshift(431)
+
+// console.log(array)
+
+// array.splice(1, 0, 34, "Naga")
+
+// // array.splice("starting Index", "number of elements will be removed", "element which will be add")
+
+// console.log(array)
+
+// array = [...array, ["sakthi", "siva", "subramani"]]
+
+// console.log(array)
+
+// console.log(array[9][1])
 
 array = [3, "string", 5.32, true, undefined, null, {name: "John", age: 15}]
 
-console.log(array)
+// console.log(array[6].name)
 
-console.log(array.pop())
-console.log(array)
+// /* ================================================ */
 
-console.log(array.shift())
-console.log(array)
+// let i = 0;
 
-array.push(143)
-array.unshift(431)
+// const l = array.length // 7
 
-console.log(array)
+// for (i ; i < l; i++) {
+//     console.log(array[i])
+// }
 
-array.splice(1, 0, 34, "Naga")
+for (let j of array ) {
+    console.log(j)
+}
 
-// array.splice("starting Index", "number of elements will be removed", "element which will be add")
-
-console.log(array)
-
-array = [...array, ["sakthi", "siva", "subramani"]]
-
-console.log(array)
-
-console.log(array[9][1])
-
-array = [3, "string", 5.32, true, undefined, null, {name: "John", age: 15}]
-
-console.log(array[6].name)
+for (j in array) {
+    console.log(j)
+}
---
+++ objects.js
@@ -0,0 +1,81 @@
+// const student = {
+//     firstName: "Bharani",
+//     class: 10,
+//     marks: {
+//         maths: 96,
+//         english: 89
+//     },
+//     result: function () {
+//         let sureName = "Lakshmi"
+
+//         console.log(`${this.firstName} ${sureName} completed his 10th standard with 96%`)
+//     }
+// }
+
+// for (let i in student) {
+//     console.log(i)
+// }
+
+// let k = Object.keys(student)
+// let l = Object.values(student)
+// let m = Object.entries(student)
+
+// console.log(m)
+// for (i of student) {
+//     console.log(i)
+// }
+
+// console.log(typeof student)
+
+// console.log(student.firstName)
+
+// console.log(student.marks.maths)
+
+// console.log(student.result())
+
+
+function Person (name = "Sasi", age = 22) {
+    this.name = name,
+    this.age = age
+}
+
+const person = new Person()
+
+const person2 = new Person("Ravi", 18)
+
+
+// Student
+
+s1 = {
+    name: "jai",
+    class: 12
+}
+
+s2 = {
+    name: "suguna",
+    class: 8
+}
+
+function Student (name, age) {
+    this.name = name,
+    this.class = age
+}
+
+const s1 = new Student("jai", 12)
+
+const s2 = new Student("suguna", 8)
+
+person2.gender = "Male"
+
+// console.log(person2)
+
+Person.prototype.profession = "student"
+
+console.log(person)
+console.log(Object.keys(person2))
+
+
+
+
+// console.log(Person())
+
