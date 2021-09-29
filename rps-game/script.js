13e3a0df10b8bf792393192ba5189e416f0c5891
VVJS-22 <s.jayeshchennai@gmail.com>
2021-09-29 19:02:43 +0530

    11th class

--- rps-game/script.js
+++ rps-game/script.js
@@ -11,6 +11,17 @@ const scissors = document.querySelector("#scissors")
 
 const choices = ["rock", "paper", "scissors"]
 
+/*
+
+choices[0] // rock
+choices[1] // paper
+choices[2] // scissors
+choices[3] // undefined
+
+*/
+
+
+
 const scoreDatabase = {
     "rock" : {
         "rock": 0.5, "paper": 0, "scissors": 1 
@@ -24,3 +35,56 @@ const scoreDatabase = {
 }
 
 // =========== Database ================//
+
+
+// ============= Main game ============== //
+
+const BotSelection = () => {
+
+    const random = Math.floor(Math.random()*3) // returns 0 || 1 || 2
+
+    return choices[random];
+
+}
+
+const Score = (yourChoice, botChoice) => {
+
+    return scoreDatabase[yourChoice][botChoice];
+
+}
+
+const GameMessage = (gameScore) => {
+    let result;
+    switch(gameScore) {
+        case 1:
+            result = { message: "You Won!", color: "green" }
+            break
+        case 0.5:
+            result = { message: "You Tied!", color: "yellow" }
+            break
+        case 0:
+            result = { message: "You Lost!", color: "red" }
+            break
+        default:
+            result = { message: "GameScore undefined", color: "grey"}
+    }
+    return result
+}
+
+const RemoveImages = () => {
+    rock.remove()
+    paper.remove()
+    scissors.remove()
+}
+
+const RpsGame = (choice) => {
+    const yourChoice = choice.id;
+    const botChoice = BotSelection();
+    const gameScore = Score(yourChoice, botChoice);
+    const finalMessage = GameMessage(gameScore);
+    console.log(yourChoice, botChoice, gameScore,finalMessage)
+    RemoveImages() 
+}
+
+// ============= Main game ============== //
+
